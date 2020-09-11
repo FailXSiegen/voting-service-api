c2e6d0acda9d8ac8f979a6190080d3e03e7584cb
Wolf Utz <wpu@hotmail.de>
2020-09-11 23:10:51 +0200

    Fix poll mutation script path

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,7 +1,7 @@
 import eventQueries from './resolver/queries/event'
 import organizerMutations from './resolver/mutation/organizer.js'
 import eventMutations from './resolver/mutation/event'
-import pollMutations from './resolver/queries/poll'
+import pollMutations from './resolver/mutation/poll'
 import pollResolvers from './resolver/poll/poll'
 
 export default {
