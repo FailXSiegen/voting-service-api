b6e7fc133da08bd3d7b7c04403aba8a755942975
Wolf Utz <wpu@hotmail.de>
2020-09-15 23:05:50 +0200

    Add event-user login
    * Remove bcrypt and add argon2

---
+++ src/auth/login/login-event-user.js
@@ -0,0 +1,60 @@
+import {
+  findOneByUsernameAndEvenId,
+  findOneById,
+  create,
+  update
+} from '../../repository/event-user-repository'
+import { generateJwt } from '../../lib/jwt-auth'
+import * as jwt from 'jsonwebtoken'
+import { addRefreshToken } from './refresh-token'
+import { verify } from '../../lib/crypto'
+import AuthenticationError from '../../errors/AuthenticationError'
+
+async function buildNewEventUserObject (username, password, email, displayName, eventId) {
+  return {
+    username,
+    password,
+    email: typeof email === 'string' ? email : '',
+    publicName: displayName,
+    allowToVote: false,
+    online: false,
+    coorganizer: false,
+    verified: false,
+    eventId
+  }
+}
+
+export default async function loginEventUser ({ username, password, email, displayName, eventId }) {
+  let eventUser = await findOneByUsernameAndEvenId(username, eventId)
+  if (!eventUser) {
+    // create new event user.
+    const newEventUserId = await create(
+      await buildNewEventUserObject(username, password, email, displayName, eventId)
+    )
+    // Fetch newly created event user.
+    eventUser = await findOneById(newEventUserId)
+    if (eventUser === null) {
+      throw new Error('Could not create new user!')
+    }
+  } else {
+    // Verify password.
+    const isAuthenticated = await verify(password, eventUser.password)
+    if (!isAuthenticated) {
+      throw new AuthenticationError()
+    }
+    if (eventUser.publicName !== displayName) {
+      // Update display name.
+      eventUser.publicName = displayName
+      await update(eventUser)
+    }
+  }
+  // Create jwt and refresh token.
+  const refreshToken = await addRefreshToken('event-user', eventUser.id)
+  const claims = {
+    user: { id: eventUser.id, type: 'event-user' },
+    role: 'event-user'
+  }
+  const token = await generateJwt(claims)
+  const decodedToken = await jwt.verify(token, process.env.JWT_SECRET)
+  return { token, decodedToken, refreshToken }
+}
