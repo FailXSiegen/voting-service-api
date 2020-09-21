5337c6e54871774cdab1f610c7fb01e06f674048
Wolf Utz <wpu@hotmail.de>
2020-09-21 20:30:01 +0200

    Update updateEventUserAccessRights subscriber payload
    Also remove findEventUserByEvent query and add eventUsers instead

--- src/graphql/resolver/queries/event-user.js
+++ src/graphql/resolver/queries/event-user.js
@@ -1,6 +1,12 @@
-import { findOneById } from '../../../repository/event-user-repository'
+import {
+  findEventUserByEventId,
+  findOneById
+} from '../../../repository/event-user-repository'
 
 export default {
+  eventUsers: async (_, args, context) => {
+    return await findEventUserByEventId(args.eventId)
+  },
   eventUser: async (_, args, context) => {
     return await findOneById(args.id)
   }
