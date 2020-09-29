54d97d4a837759ec18382f48605f1990005b32e9
FailXSiegen <info@failx.de>
2020-09-29 18:45:25 +0200

    add new type for open polls (with poll_result entry but not closed = 1) (#42)
    * add new type for open polls (with poll_result entry but not closed = 1)
    
    * rename to findActivePoll, there should be only one active Poll at a time per event
    
    * closePollResult with pollId instead of pollResultId
    
    * fix db query if no answers given yet
    
    * move query from poll to poll_result repository
    
    * fix mutator to close poll manually
    
    * fix mutate on auto-close
    
    Co-authored-by: Felix Herrmann <felix.herrmann@riconet.de>

--- src/graphql/resolver/queries/poll-result.js
+++ src/graphql/resolver/queries/poll-result.js
@@ -1,7 +1,10 @@
-import { findClosedPollResults } from '../../../repository/poll/poll-result-repository'
+import { findClosedPollResults, findActivePoll } from '../../../repository/poll/poll-result-repository'
 
 export default {
   pollResult: async (_, { eventId }, context) => {
     return await findClosedPollResults(eventId)
+  },
+  activePoll: async (_, { eventId }, context) => {
+    return await findActivePoll(eventId)
   }
 }
