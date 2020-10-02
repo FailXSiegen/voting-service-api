59e8b84f6ad84a042dfce87c3652415bb8b8c19f
Wolf Utz <wpu@hotmail.de>
2020-10-02 21:39:36 +0200

    Update .env and jwt auth middleware
    * Add endpoints as env vars
    * Add graphql path condition for jwt auth

--- src/middleware/authenticate.js
+++ src/middleware/authenticate.js
@@ -2,7 +2,7 @@ import * as jwt from 'jsonwebtoken'
 import AuthenticationError from '../errors/AuthenticationError'
 
 const authenticate = async (resolve, root, args, context, info) => {
-  if (process.env.ENABLE_JWT === '1') {
+  if (process.env.ENABLE_JWT === '1' && context.req.path === process.env.GRAPHQL_ENDPOINT) {
     try {
       const authHeader = context.req.get('Authorization')
       const token = authHeader.split(' ')[1] || ''
