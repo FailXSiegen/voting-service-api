ffc2103f54eb52ed2e3eb7f6a8193bef69158584
Felix Herrmann <felix.herrmann@riconet.de>
2020-10-03 17:37:26 +0200

    fix for reload after pollAnswerSubmit and resubmit

--- src/graphql/resolver/active-poll-event-user/active-poll-event-user.js
+++ src/graphql/resolver/active-poll-event-user/active-poll-event-user.js
@@ -1,7 +1,11 @@
 import { findOneById } from '../../../repository/poll/poll-repository'
+import { findByPollResultId } from '../../../repository/poll/poll-user-voted-repository'
 
 export default {
   poll: async ({ poll }) => {
     return await findOneById(poll)
+  },
+  pollUserVoted: async ({ pollResultId }) => {
+    return await findByPollResultId(pollResultId)
   }
 }
