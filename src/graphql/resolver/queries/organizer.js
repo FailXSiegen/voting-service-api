4402ccd602f15ee0440903b8eac889e08d0ea762
FailXSiegen <info@failx.de>
2020-12-13 14:17:26 +0100

    Add update organizer mutation and schema

--- src/graphql/resolver/queries/organizer.js
+++ src/graphql/resolver/queries/organizer.js
@@ -1,6 +1,9 @@
-import { findOrganizers } from '../../../repository/organizer-repository'
+import { findOrganizers, findOneById } from '../../../repository/organizer-repository'
 
 export default {
+  organizer: async (_, { organizerId }, context) => {
+    return await findOneById(organizerId)
+  },
   organizers: async (_, args, context) => {
     return await findOrganizers()
   }
