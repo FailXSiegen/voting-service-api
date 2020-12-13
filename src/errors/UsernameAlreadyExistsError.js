1174aaedda31c0f8b525661f8b0b6f21ba1a0199
FailXSiegen <info@failx.de>
2020-12-13 20:39:37 +0100

    remove obsolet error handling and message.

--- src/errors/UsernameAlreadyExistsError.js
+++
@@ -1,7 +0,0 @@
-import { createError } from 'apollo-errors'
-
-const UsernameAlreadyExistsError = createError('UsernameAlreadyExistsError', {
-  message: 'The provided username already exists.'
-})
-
-export default UsernameAlreadyExistsError
