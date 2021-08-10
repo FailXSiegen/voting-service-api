c082c8546e73641730e79d802ac2642fd6d8b165
FailXSiegen <info@failx.de>
2021-08-10 14:30:53 +0200

    update event resolver

--- src/request/event/meeting-resolver.js
+++ src/request/event/meeting-resolver.js
@@ -2,6 +2,9 @@ import { VideoConferenceType } from '../../enum'
 import { findOneById as findOneZoomMeetingById } from '../../repository/meeting/zoom-meeting-repository'
 
 export default async function (event) {
+  if (!event.videoConferenceConfig) {
+    return event
+  }
   const config = JSON.parse(event.videoConferenceConfig)
   switch (config.type) {
     case VideoConferenceType.ZOOM:
