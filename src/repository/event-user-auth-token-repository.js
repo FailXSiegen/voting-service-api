b8944572d70b02ca99ba069b5b799e78308823dd
Wolf Utz <w.utz@omega-code.de>
2023-11-26 20:21:48 +0100

    Fix delete event user call

--- src/repository/event-user-auth-token-repository.js
+++ src/repository/event-user-auth-token-repository.js
@@ -1,5 +1,17 @@
-import { query, insert } from "./../lib/database";
+import { query, insert, remove as removeQuery } from "./../lib/database";
 import { getCurrentUnixTimeStamp } from "../lib/time-stamp";
+
+/**
+ * @param {number} eventUserId
+ * @returns {object|null}
+ */
+export async function findOneByEventUserId(eventUserId) {
+  const result = await query(
+    "SELECT * FROM event_user_auth_token WHERE event_user_id = ?",
+    [eventUserId],
+  );
+  return Array.isArray(result) ? result[0] || null : null;
+}
 
 /**
  * @param {string} token
@@ -28,6 +40,7 @@ export async function findOneInactiveByToken(token) {
 /**
  * @param {string} token
  * @param {number} eventUserId
+ * @return {number|null}
  */
 export async function create(token, eventUserId) {
   return await insert("event_user_auth_token", {
@@ -43,8 +56,16 @@ export async function create(token, eventUserId) {
  * @param {string} newToken
  */
 export async function activate(token, newToken) {
-  return await query(
+  await query(
     "UPDATE event_user_auth_token SET active = ?, token = ? WHERE token = ?",
     [true, newToken, token],
   );
+}
+
+/**
+ * @param {number} id
+ * @returns {boolean}
+ */
+export async function remove(id) {
+  return removeQuery("event_user_auth_token", id);
 }
