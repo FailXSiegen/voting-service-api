b6c327b2bf81b9b75fd259098f1de66f83dd2193
Wolf-Peter Utz <wolf@utz-it.de>
2025-02-02 15:59:01 +0100

    Add `pollAnswers` to type `ActivePollEventUser` (#76)

--- src/graphql/resolver/active-poll-event-user/active-poll-event-user.js
+++ src/graphql/resolver/active-poll-event-user/active-poll-event-user.js
@@ -1,5 +1,6 @@
 import { findOneById } from "../../../repository/poll/poll-repository";
 import { findByPollResultId } from "../../../repository/poll/poll-user-voted-repository";
+import { findByPollResultId as findAnswersByPollResultId } from "../../../repository/poll/poll-answer-repository";
 import { findByEventId } from "../../../repository/poll/poll-user-repository";
 
 export default {
@@ -11,5 +12,13 @@ export default {
   },
   pollUser: async ({ poll }) => {
     return await findByEventId(poll);
+  },
+  pollAnswers: async ({ poll, pollResultId }) => {
+    const { type } = await findOneById(poll);
+    // if the poll type is not public, do not return the answers.
+    if (type !== 1) {
+      return null;
+    }
+    return await findAnswersByPollResultId(pollResultId);
   },
 };
